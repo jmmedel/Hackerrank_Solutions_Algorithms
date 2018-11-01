5c26385cb9b94e65351f28f28d2cbcf050ab8d5a
kjmkagaya <jm.kagaya@gmail.com>
2018-11-01 11:29:50 +0900

    Done
    done

---
+++ 000_30days_code/day8/answer.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+
+import java.util.Scanner;
+
+/**
+ * @author Kagaya john medel 
+ *
+ */
+public class answer {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+		in.close();
+		for (int i = n - 1; i >= 0; i--) {
+
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
