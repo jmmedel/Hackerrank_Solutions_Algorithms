9ce78306e3ca0b4b8ea2aac51b653e48f8dc8b94
kjmkagaya <jm.kagaya@gmail.com>
2018-11-02 09:36:16 +0900

    Done
    Done

---
+++ 000_30days_code/day11/answer.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * @author Kagaya john medel 
+ *
+ */
+public class answer {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.close();
+		int r = n, counter = 0, maxOne = 0;
+		String s = "";
+		while (n > 0) {
+
+			r = n % 2;
+			if (r == 1) {
+				counter++;
+				if (counter > maxOne) {
+					maxOne = counter;
+				}
+			} else {
+				counter = 0;
+			}
+			s = r + s;
+			n = n / 2;
+
+		}
+		System.out.println(maxOne);
+
+	}
+}
